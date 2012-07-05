b6f0c66561073fc9d585ed81ce9c454336d21520
Thabo Fletcher <tha.bo.fletcher@gmail.com>
2012-07-05 09:59:54 -0700

    different test output and added .gitignore

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using LazyRabbit;
 
 namespace TestConsole
 {
@@ -11,7 +13,48 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            TestDNS();
+            //SendTestMail();
 
+            Console.Read();
+        }
+
+        private static void TestDNS()
+        {
+            var dnsServer = DefaultDns.Current;
+            if (dnsServer == null)
+            {
+                Console.WriteLine("Invalid DNS, reverting to Google");                
+            }
+            else
+            {
+                Console.WriteLine("MXs according to your nameserver:");
+                try
+                {
+                    PrintMXs("gmail.com", dnsServer);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("failed: " + exc.ToString());
+                }
+            }
+
+            Console.WriteLine("MXs according to Google:");
+            dnsServer = IPAddress.Parse("8.8.8.8");
+            PrintMXs("gmail.com", dnsServer);
+        }
+
+        private static void PrintMXs(string host, IPAddress dnsServer)
+        {
+            var mxs = Bdev.Net.Dns.Resolver.MXLookup(host, dnsServer);
+            foreach (var mx in mxs)
+            {
+                Console.WriteLine(mx.DomainName);
+            }
+        }
+
+        private static void SendTestMail()
+        {
             var msg = new MailMessage
             {
                 From = new MailAddress("me@" + Environment.MachineName),
@@ -21,10 +64,8 @@ namespace TestConsole
 
             msg.To.Add(new MailAddress("you@gmail.com"));
 
-            var ka = new LazyRabbit.OnehopMail();
+            var ka = new OnehopMail();
             ka.Send(msg, exc => { Console.WriteLine(exc.ToString()); });
-
-            Console.Read();
         }
     }
 }
